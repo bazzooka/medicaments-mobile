6449b030eace11d4f2f5d5769aab9e0a10bd783a
Joe <bazzooka@gmail.com>
2015-07-06 11:35:54 +0200

    Update styles and add new components

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,55 @@
-var main = require('../jsx/main.jsx');
+// var main = require('../jsx/main.jsx');
+var ViewManager = require('./components/ViewManager.js');
+var NavBar = require('./components/NavBar.js');
+var SideMenu = require('./components/SideMenu.js');
+var HomePage = require('./pages/HomePage.js'); 
+var FavoritePage = require('./pages/FavoritePage.js'); 
+
+class App {
+
+	constructor(options){
+		this.options = options;
+		this.routes = { 
+			'/': HomePage.show 
+		}
+		this.init();
+	} 
+
+	init(){
+		console.log("Init"); 
+		this.viewManager = new ViewManager({
+			containerId: "page-container",
+			pages: [
+				{reference: HomePage, route: '/', id: "home-page", isDefaultPage: true},
+				{reference: FavoritePage, route: '/favorite', id: "favorite-page"}
+			]
+		});
+
+		this.sideMenu = new SideMenu({sideMenuId: "menu-container"});
+	}
+}
+
+var myAPP = new App();
+
+// module.exports = App;
+
+// var author = function () { console.log("author"); };
+// var books = function () { console.log(arguments); };
+// var viewBook = function (bookId) {
+// console.log("viewBook: bookId is populated: " + bookId);
+// };
+
+// var routes = {
+// '/author': author,
+// '/books': [books, function() {
+//   console.log("An inline route handler.");
+// }],
+// '/books/view/:bookId': viewBook
+// };
+
+// var router = Router(routes);
+
+// router.init();
+
+
+
